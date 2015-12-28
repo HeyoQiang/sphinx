6debba1d54e2fa402477be1385d454543624129a
Yongqiang <he.yongqiang@h3c.com>
2015-12-28 17:41:23 +0800

    123

--- tests/path.py
+++ tests/path.py
@@ -5,7 +5,7 @@
     ~~~~
 
     :copyright: Copyright 2007-2015 by the Sphinx team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
+    :license: BSD, see LICENSE for details
 """
 import os
 import sys
